3684addb31e4399efd1f92594b2bee6ac502e35d
RDz <rdzmailbox1@gmail.com>
2026-05-15 01:08:36 +0500

    Jwt token work

--- src/main/java/com/example/javafinalproject/JWT/JumagulovRolanJwtUtils.java
+++ src/main/java/com/example/javafinalproject/JWT/JumagulovRolanJwtUtils.java
@@ -1,9 +1,16 @@
 package com.example.javafinalproject.JWT;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
+@Builder
 @Component
 @Slf4j
 public class JumagulovRolanJwtUtils {
@@ -13,6 +20,35 @@ public class JumagulovRolanJwtUtils {
     @Value("${app.jwt.expirationMs")
     private int jwtExpirationMs;
 
+    public String generateJwtToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
 
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser()
+                    .verifyWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                    .build()
+                    .parseSignedClaims(authToken);
+            return true;
+        } catch (Exception e) {
+            log.error("Invalid JWT");
+        }
+        return false;
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
 
 }
