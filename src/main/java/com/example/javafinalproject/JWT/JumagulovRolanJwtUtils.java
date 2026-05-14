9fecec90124769f35b06ee013653a736627da1fe
RDz <rdzmailbox1@gmail.com>
2026-05-15 00:02:40 +0500

    Added the secret key + expiration time

--- src/main/java/com/example/javafinalproject/JWT/JumagulovRolanJwtUtils.java
+++ src/main/java/com/example/javafinalproject/JWT/JumagulovRolanJwtUtils.java
@@ -1,11 +1,18 @@
 package com.example.javafinalproject.JWT;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class JumagulovRolanJwtUtils {
-    
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt.expirationMs")
+    private int jwtExpirationMs;
+
+
 
 }
