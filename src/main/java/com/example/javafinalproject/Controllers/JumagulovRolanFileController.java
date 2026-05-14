9202da242556015bcf48c284f59cc0b7895e9fd6
RDz <rdzmailbox1@gmail.com>
2026-05-14 23:17:27 +0500

    File downloading and uploading

--- src/main/java/com/example/javafinalproject/Controllers/JumagulovRolanFileController.java
+++ src/main/java/com/example/javafinalproject/Controllers/JumagulovRolanFileController.java
@@ -1,15 +1,17 @@
 package com.example.javafinalproject.Controllers;
 
-
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.FileSystemResource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @Slf4j
@@ -22,8 +24,24 @@ public class JumagulovRolanFileController {
         try{
             File directory = new File(uploadDir);
             if (!directory.exists()) directory.mkdir();
-        }
 
+        String filePath = uploadDir + file.getOriginalFilename();
+        file.transferTo(new File(filePath));
+
+        log.info("File uploaded");
+        return ResponseEntity.ok("File uploaded successfully");
+    } catch (IOException e) {
+        return ResponseEntity.status(500).body("Could not upload file");
+    }
+}
+    @GetMapping("/download/{filename}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename) {
+        Path path = Paths.get(uploadDir).resolve(filename);
+        Resource resource = new FileSystemResource(path);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
 }
